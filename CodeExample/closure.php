c274b43657574252255dc4ad7cc41c63d78a5391
Michael Petrov <mpetrov@srt-llc.ru>
2024-04-08 09:45:53 +0300

    Commit

--- CodeExample/closure.php
+++ CodeExample/closure.php
@@ -15,3 +15,42 @@ $a = 44; // не применится, если что. Потому что за
 
 $result = $closure($x);
 echo $result . PHP_EOL;
+
+class ClosureThis
+{
+    private int $a = 100;
+
+    private function multiple(): int
+    {
+        return $this->a * 10;
+    }
+    public function handle(Closure $closure)
+    {
+        return $closure($this->multiple());
+    }
+}
+
+class ClosureHandler
+{
+    private int $a = 20;
+
+    public function call(): Closure
+    {
+        return function () {
+            return get_class($this) . ' ' . $this->a;
+        };
+    }
+}
+
+$closureThis = new ClosureThis();
+$closureHandler = new ClosureHandler();
+
+$f = $closureHandler->call();
+
+echo $closureThis->handle($f) . PHP_EOL;
+
+$f2 = $f->bindTo($closureThis, $closureThis);
+
+echo $closureThis->handle($f2) . PHP_EOL;
+
+
